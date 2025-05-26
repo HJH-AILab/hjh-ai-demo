e50c2ed80c1830c8fa40787a608b7f7dafc8c1f3
zhangchunsheng <zhangchunsheng423@gmail.com>
2025-05-26 19:43:55 +0800

    update

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -14,18 +14,19 @@ class PagesController extends Controller
 
     /**
      * 首页展示,音图话
-     */ 
-    public function index(){
-        $m = Music::Released()->orderBy('id','desc')->first();
-        if($m===null){
+     */
+    public function index()
+    {
+        $m = Music::Released()->orderBy('id', 'desc')->first();
+        if ($m === null) {
             $id = 1;
-        }else{
+        } else {
             $id = $m->image_id;
         }
         $image = Image::findOrFail($id);
         $music = $image->musics()->Released()->get();
         $list = [];
-        foreach($music as $key => $row){
+        foreach ($music as $key => $row) {
             $list[$key]['name'] = $row->name;
             $list[$key]['artist'] = $row->artist;
             $list[$key]['cover'] = $row->cover;
@@ -33,7 +34,8 @@ class PagesController extends Controller
             $list[$key]['desc'] = $row->desc;
             $list[$key]['auth'] = $row->user->name;
         }
-        return view('pages.index',
+        return view(
+            'pages.index',
             [
                 'image' => $image,
                 'list' => json_encode($list),
@@ -45,12 +47,13 @@ class PagesController extends Controller
 
     /**
      * 首页展示,音图话
-     */ 
-    public function music($id){
+     */
+    public function music($id)
+    {
         $image = Image::findOrFail($id);
         $music = $image->musics()->Released()->get();
         $list = [];
-        foreach($music as $key => $row){
+        foreach ($music as $key => $row) {
             $list[$key]['name'] = $row->name;
             $list[$key]['artist'] = $row->artist;
             $list[$key]['cover'] = $row->cover;
@@ -59,7 +62,8 @@ class PagesController extends Controller
             $list[$key]['auth'] = $row->user->name;
         }
 
-        return view('pages.index',
+        return view(
+            'pages.index',
             [
                 'image' => $image,
                 'list' => json_encode($list),
@@ -74,9 +78,10 @@ class PagesController extends Controller
      */
     public function list()
     {
-        
+
         $res = Image::has('musics')->orderBy('created_at', 'desc')->Released()->simplePaginate(24);
-        return view('pages.list', 
+        return view(
+            'pages.list',
             [
                 'images' => $res,
                 'title' => '音图话'
@@ -91,18 +96,19 @@ class PagesController extends Controller
     public function root()
     {
         $page = request('page');
-        if(!$page){
+        if (!$page) {
             $page = 1;
         }
-        $cacheKey = "images:list:".$page;
+        $cacheKey = "images:list:" . $page;
         if (Redis::exists($cacheKey)) {
             $res = Redis::get($cacheKey);
             $res = unserialize($res);
         } else {
-            $res = Image::orderBy('created_at', 'desc')->Released()->simplePaginate(24,['*'],'page',$page);
-            Redis::setex($cacheKey, 3600*mt_rand(1,24), serialize($res));
+            $res = Image::orderBy('created_at', 'desc')->Released()->simplePaginate(24, ['*'], 'page', $page);
+            Redis::setex($cacheKey, 3600 * mt_rand(1, 24), serialize($res));
         }
-        return view('pages.root', 
+        return view(
+            'pages.root',
             [
                 'images' => $res,
                 'title' => '图片'
@@ -117,25 +123,26 @@ class PagesController extends Controller
     public function show($id)
     {
         Image::findOrFail($id)->increment('views');
-        $cacheKey = "images:show:".$id;
+        $cacheKey = "images:show:" . $id;
         if (Redis::exists($cacheKey)) {
             $res = Redis::get($cacheKey);
             $res = unserialize($res);
         } else {
             $res = Image::findOrFail($id);
-            Redis::setex($cacheKey, 3600*mt_rand(1,24), serialize($res));
+            Redis::setex($cacheKey, 3600 * mt_rand(1, 24), serialize($res));
         }
 
-        $cacheUserKey = "images:morelist:".$id;
+        $cacheUserKey = "images:morelist:" . $id;
         if (Redis::exists($cacheUserKey)) {
             $more = Redis::get($cacheUserKey);
             $more = unserialize($more);
         } else {
             $more = Image::inRandomOrder()->Released()->limit(12)->get();
-            Redis::setex($cacheUserKey, 3600*mt_rand(1,24), serialize($more));
+            Redis::setex($cacheUserKey, 3600 * mt_rand(1, 24), serialize($more));
         }
 
-        return view('pages.show',
+        return view(
+            'pages.show',
             [
                 'image' => $res,
                 'more' => $more
@@ -145,10 +152,11 @@ class PagesController extends Controller
 
     /**
      * 网站版本权限说明
-     */ 
+     */
     public function license()
     {
-        return view('pages.license',
+        return view(
+            'pages.license',
             [
                 'title' => '版本说明'
             ]
@@ -157,10 +165,11 @@ class PagesController extends Controller
 
     /**
      * 我为图片配音乐说明
-     */ 
+     */
     public function play()
     {
-        return view('pages.play',
+        return view(
+            'pages.play',
             [
                 'title' => '玩法说明'
             ]
@@ -172,29 +181,30 @@ class PagesController extends Controller
      */
     public function user($id)
     {
-        $cacheKey = "images:user:".$id;
+        $cacheKey = "images:user:" . $id;
         if (Redis::exists($cacheKey)) {
             $user = Redis::get($cacheKey);
             $user = unserialize($user);
         } else {
             $user = User::findOrFail($id);
-            Redis::setex($cacheKey, 3600*mt_rand(1,24), serialize($user));
+            Redis::setex($cacheKey, 3600 * mt_rand(1, 24), serialize($user));
         }
-        
+
         $page = request('page');
-        if(!$page){
+        if (!$page) {
             $page = 1;
         }
-        $cacheUserKey = "images:userlist:".$id.":".$page;
+        $cacheUserKey = "images:userlist:" . $id . ":" . $page;
         if (Redis::exists($cacheUserKey)) {
             $res = Redis::get($cacheUserKey);
             $res = unserialize($res);
         } else {
-            $res = $user->images()->orderBy('created_at', 'desc')->Released()->simplePaginate(24,['*'],'page',$page);
-            Redis::setex($cacheUserKey, 3600*mt_rand(1,24), serialize($res));
+            $res = $user->images()->orderBy('created_at', 'desc')->Released()->simplePaginate(24, ['*'], 'page', $page);
+            Redis::setex($cacheUserKey, 3600 * mt_rand(1, 24), serialize($res));
         }
-        
-        return view('pages.user',
+
+        return view(
+            'pages.user',
             [
                 'images' => $res,
                 'user' => $user,
@@ -208,9 +218,10 @@ class PagesController extends Controller
      */
     public function tag($name)
     {
-        
-        $res = Image::where('keywords','like',"%$name%")->orderBy('created_at', 'desc')->Released()->simplePaginate(24);
-        return view('pages.tag',
+
+        $res = Image::where('keywords', 'like', "%$name%")->orderBy('created_at', 'desc')->Released()->simplePaginate(24);
+        return view(
+            'pages.tag',
             [
                 'images' => $res,
                 'title' => $name,
@@ -227,7 +238,8 @@ class PagesController extends Controller
         $query = $request->input('query');
         $res = Image::search($query)->simplePaginate(24);
 
-        return view('pages.search',
+        return view(
+            'pages.search',
             [
                 'images' => $res,
                 'title' => $query,
@@ -235,5 +247,4 @@ class PagesController extends Controller
             ]
         );
     }
-
 }
