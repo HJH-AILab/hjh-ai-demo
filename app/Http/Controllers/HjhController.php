e660dfa877d520faedce5c198d2ed292e2214d94
zhangchunsheng <zhangchunsheng423@gmail.com>
2025-05-27 15:58:29 +0800

    update

---
+++ app/Http/Controllers/HjhController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Services\HjhCloudService;
+use Illuminate\Http\Request;
+use Auth;
+use App\Models\Image;
+use App\Models\Music;
+use Image as Intervention;
+use Storage;
+
+class HjhController extends Controller
+{
+
+    /**
+     * 添加验证中间件
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+     /**
+     * showWorkflowForm
+     * @return void
+     */
+    public function showWorkflowForm()
+    {
+        $workflows = HjhCloudService::getInstance()->getWorkflows();
+        if(!empty($workflows)) {
+            $workflows = $workflows['list'];
+        }
+        return view('hjh.workflow', [
+                'workflows' => $workflows,
+                'title' => '好机绘工作流列表'
+            ]);
+    }
+    
+}
---
+++ resources/views/hjh/workflow.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+@section('title', $title)
+@section('content')
+<div class="row masonry">
+    @foreach($workflows as $workflow)
+      <div>
+        <h3 class="text-center">{{ $workflow->name }}</h3>
+        @foreach($workflow->workflows as $workflow1)
+          <div class="col-xl-2 col-lg-2 col-md-4 col-sm-6 col-12 item">
+            <div class="card">
+              <img src="{{ $workflow1->effect_thumbnail_image }}" class="card-img-top" alt="{{ $workflow1->name }}">
+            </div>
+          </div>
+        @endforeach
+      </div>
+    @endforeach
+</div>
+
+@push('pbl-js')
+<script src="https://tiangong2.wepromo.cn/js/jquery.lazyload.min.js"></script>
+<script src="https://tiangong2.wepromo.cn/js/masonry.pkgd.min.js"></script>
+<script src="https://tiangong2.wepromo.cn/js/imagesloaded.pkgd.min.js"></script>
+<script>
+  $(function(){
+      $(".lazyload").lazyload();//图片懒加载
+      $('.masonry').masonry({
+        itemSelector: '.item',
+      });
+      var $container = $('.masonry');
+      $container.imagesLoaded(function(){
+        $container.masonry({
+          itemSelector: '.item',
+          isAnimated: true,
+        });
+      });
+  });
+</script>
+@endpush
+@stop
