61e03fc581615710fcddc57e4282cc2ee5c17dac
zhangchunsheng <zhangchunsheng423@gmail.com>
2025-05-28 12:11:31 +0800

    update

---
+++ database/migrations/2025_05_28_120659_add_workflow_to_images.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDescToImages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->integer('source')->comment('来源 0-默认 1-好机绘')->defalut(0);
+            $table->integer('workflow_id')->comment('工作流ID')->defalut(0);
+            $table->string('workflow_name')->comment('工作流名称')->defalut("");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->dropColumn('source');
+            $table->dropColumn('workflow_id');
+            $table->dropColumn('workflow_name');
+        });
+    }
+}
