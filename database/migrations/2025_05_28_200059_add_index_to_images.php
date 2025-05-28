aaadd8a86ceebc9a02142a6e01cb5a8b3b352013
zhangchunsheng <zhangchunsheng423@gmail.com>
2025-05-28 20:05:06 +0800

    update

---
+++ database/migrations/2025_05_28_200059_add_index_to_images.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIndexToImages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->index(array('source'), 'idx_source');
+            $table->index(array('workflow_id'), 'idx_workflow_id');
+            $table->index(array('user_id'), 'idx_user_id');
+            $table->index(array('desc'), 'idx_desc');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->dropIndex('idx_source');
+            $table->dropIndex('idx_workflow_id');
+            $table->dropIndex('idx_user_id');
+            $table->dropIndex('idx_desc');
+        });
+    }
+}
