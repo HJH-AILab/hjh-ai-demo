31b6c41e252391518d4ccd3987b6293050813785
zhangchunsheng <zhangchunsheng423@gmail.com>
2025-05-28 13:23:05 +0800

    update

--- database/migrations/2025_05_28_121529_create_hjh_draw_task_table.php
+++ database/migrations/2025_05_28_121529_create_hjh_draw_task_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateHjhImagesTable extends Migration
+class CreateHjhDrawTaskTable extends Migration
 {
     /**
      * Run the migrations.
