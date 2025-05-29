ec15433805cc72ed838a7496b092652d61785b87
zhangchunsheng <zhangchunsheng423@gmail.com>
2025-05-29 13:43:27 +0800

    update

--- app/Http/Controllers/HjhController.php
+++ app/Http/Controllers/HjhController.php
@@ -173,7 +173,7 @@ class HjhController extends Controller
         fastcgi_read_timeout 600;
          */
         //ini_set("max_execution_time", "300");
-        set_time_limit(300);
+        //set_time_limit(300);
         $data = $request->all();
         $taskNo = $data['task_no'] ?? '';
         if(empty($taskNo)) {
