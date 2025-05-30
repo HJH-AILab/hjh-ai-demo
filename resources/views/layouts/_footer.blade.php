deb7add89b3bdd39c7fced42947e9e8e9adaa7ad
zhangchunsheng <zhangchunsheng423@gmail.com>
2025-05-30 19:34:57 +0800

    update

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,10 +1,10 @@
-<footer class="footer">
+<footer class="footer" style="height: 600px;">
   <div class="container">
     <p class="float-left">
       <a href="https://beian.miit.gov.cn" target="_blank">京ICP备14011022号-3</a>
     </p>
 
-    <p class="float-right" style="height: 600px;">
+    <p class="float-right">
       <span>
         <img src="https://tiangong2.wepromo.cn/img/WechatIMG13022.jpg" width="300px" alt="企业微信" align=center />
         <img src="https://tiangong2.wepromo.cn/img/WX20250530-183744@2x.png" width="300px" alt="企业微信" align=center />
