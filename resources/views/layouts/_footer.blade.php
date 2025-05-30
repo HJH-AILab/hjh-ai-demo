3f43879b8e479a3e7b7fc86e1a17e529bdb1458e
zhangchunsheng <zhangchunsheng423@gmail.com>
2025-05-30 19:30:14 +0800

    update

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -6,8 +6,8 @@
 
     <p class="float-right">
       <span>
-        <img src="docs/WechatIMG13022.jpg" width="300px" alt="企业微信" align=center />
-        <img src="docs/WX20250530-183744@2x.png" width="300px" alt="企业微信" align=center />
+        <img src="https://tiangong2.wepromo.cn/img/WechatIMG13022.jpg" width="300px" alt="企业微信" align=center />
+        <img src="https://tiangong2.wepromo.cn/img/WX20250530-183744@2x.png" width="300px" alt="企业微信" align=center />
       </span>
       <a href="mailto:xitongtongzhi@ai-space.net">联系我们</a>
     </p>
