688d43a98630a5037feec23383eec5ef732ff2b9
zhangchunsheng <zhangchunsheng423@gmail.com>
2025-06-04 18:24:23 +0800

    update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <form id="loginform" name="loginform" method="POST" action="{{ route('register') }}" omsubmit='javascript:return false checkForm()'>
+                    <form id="loginform" name="loginform" method="POST" action="{{ route('register') }}" omsubmit='javascript:return false; checkForm()'>
                         @csrf
 
                         <div class="form-group row">
