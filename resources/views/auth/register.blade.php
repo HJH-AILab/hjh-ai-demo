de37877da9067ed88fadc2733edb0f9a099831cf
zhangchunsheng <zhangchunsheng423@gmail.com>
2025-06-05 18:00:40 +0800

    update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -130,6 +130,26 @@
         </div>
     </div>
     </div>
+    <!-- Modal -->
+    <div class="modal fade" id="modal1" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="modalLabel">好机绘产品隐私政策</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            @include('auth.agreement2')
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">关闭</button>
+            <button type="button" class="btn btn-primary" onclick="agreement1()">同意</button>
+        </div>
+        </div>
+    </div>
+    </div>
 </div>
 @endsection
 <script>
