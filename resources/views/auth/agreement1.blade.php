0e4c291cb647efd0daa6537c8c325ed493f2468a
zhangchunsheng <zhangchunsheng423@gmail.com>
2025-06-05 17:14:17 +0800

    update

--- resources/views/auth/agreement1.blade.php
+++ resources/views/auth/agreement1.blade.php
@@ -1,6 +1,6 @@
 <style type="text/css">
 	.page { padding: 1rem; }
-	.page p { margin-top: 10px; text-align: justify; orphans: 0; widows: 0; margin-bottom: 0.1in; direction: ltr; background: transparent }
+	.page p { margin-top: 10px; line-height: 115%; text-align: justify; orphans: 0; widows: 0; margin-bottom: 0.1in; direction: ltr; background: transparent }
 	.page a:link { color: #0563c1; text-decoration: underline }
 </style>
 <div class="page">
