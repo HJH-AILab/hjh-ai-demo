bdf9a99b234603cc5594f13e1521295f72330ab3
zhangchunsheng <zhangchunsheng423@gmail.com>
2025-06-06 15:46:00 +0800

    update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -135,7 +135,7 @@
     <div class="modal-dialog modal-dialog-scrollable">
         <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title" id="modalLabel">好机绘产品隐私政策</h5>
+            <h5 class="modal-title" id="modal1Label">好机绘产品隐私政策</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
             </button>
