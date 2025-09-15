2dabaf03a9e8132443c88e78b021243e15257a22
zhangchunsheng <zhangchunsheng423@gmail.com>
2025-09-15 19:06:22 +0800

    update

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -24,6 +24,7 @@
 
       <ul class="navbar-nav">
         <li><a class="nav-link" href="{{ route('hjh.image') }}">好机绘AI画廊</a></li>
+        <li><a class="nav-link" href="{{ route('hjh.video') }}">好机绘AI视频</a></li>
         <li><a class="nav-link" href="{{ route('list') }}">音图话</a></li>
         <li><a class="nav-link" href="{{ route('root') }}">超清大图</a></li>
       </ul>
