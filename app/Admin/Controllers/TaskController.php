005acff818d6b852052a027c44e24cadfeb49cb2
zhangchunsheng <zhangchunsheng423@gmail.com>
2025-09-15 18:37:39 +0800

    update

--- app/Admin/Controllers/TaskController.php
+++ app/Admin/Controllers/TaskController.php
@@ -82,23 +82,10 @@ class TaskController extends Controller
         $grid = new Grid(new Image);
 
         $grid->id('ID');
-        $grid->column('thumb640')->image('https://tiangong2.wepromo.cn/', 150, 150);
-        $grid->desc('图片说明');
-        $grid->keywords('标签');
-        $states = [
-            'on'  => ['value' => 1, 'text' => '发布', 'color' => 'success'],
-            'off' => ['value' => 0, 'text' => '撤回', 'color' => 'danger'],
-        ];
-        $grid->column('released', '发布状态')->switch($states);
-        // $grid->lens('镜头');
-        // $grid->size('大小');
-        // $grid->resolution('分辨率');
-        // $grid->aspect_ratio('宽高比');
-        // $grid->colour('颜色');
-        // $grid->user_id('user_id');
-        // $grid->thumb('thumb');
+        $grid->task_no('task_no');
+        $grid->workflow_name('workflow_name');
         $grid->created_at('创建时间')->sortable();
-        $grid->views('views')->sortable();
+        $grid->task_status('task_status')->sortable();
         // $grid->updated_at(trans('admin.updated_at'));
 
         return $grid;
@@ -115,13 +102,7 @@ class TaskController extends Controller
         $show = new Show(Image::findOrFail($id));
 
         $show->id('ID');
-        $show->lens('lens');
-        $show->size('size');
-        $show->resolution('resolution');
-        $show->aspect_ratio('aspect_ratio');
-        $show->colour('colour');
-        $show->user_id('user_id');
-        $show->thumb('thumb');
+        $show->task_no('task_no');
         $show->created_at(trans('admin.created_at'));
         $show->updated_at(trans('admin.updated_at'));
 
@@ -138,19 +119,7 @@ class TaskController extends Controller
         $form = new Form(new Image);
 
         //$form->display('ID');
-        $form->text('desc', '图片说明');
-        $form->text('lens', '镜头');
-        $form->text('size', '大小');
-        $form->text('resolution', '分辨率');
-        $form->text('aspect_ratio', '宽高比');
-        $form->text('colour', '颜色');
-        //$form->text('user_id', '创建者');
-        $form->image('thumb', '缩略图')->removable();
-        $form->image('thumb1920', '1920缩略图')->removable();
-        $form->image('thumb1280', '1280缩略图')->removable();
-        $form->image('thumb640', '640缩略图')->removable();
-        $form->tags('keywords', '标签');
-        $form->radio('released', '发布状态')->options(['0' => '暂停发布', '1' => '发布'])->default('0');
+        $form->text('task_no', 'task_no');
         // $form->display(trans('admin.created_at'));
         // $form->display(trans('admin.updated_at'));
 
